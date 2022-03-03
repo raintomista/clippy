aba2b108ed8f779444b3332f283fe103e043c8dd
Rain Tomista <rftomista@gmail.com>
2022-03-04 01:38:20 +0800

    Add click snippet to copy (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from "react";
 import { getAppData, setAppData } from "./utils";
 
 function App() {
-  const [clipboardItems, setClipboardItems] = useState<string[]>([]);
+  const [snippets, setSnippets] = useState<string[]>([]);
 
   const initializeApp = async () => {
     const appData = await getAppData();
-    setClipboardItems(appData);
-    setAppData(appData);
+    setSnippets(appData);
 
     chrome.storage.onChanged.addListener((changes) => {
       for (let change of Object.values(changes)) {
-        setClipboardItems(change.newValue);
+        setSnippets(change.newValue);
       }
     });
+  };
+
+  const copySnippet = async (snippet: string) => {
+    await navigator.clipboard.writeText(snippet);
   };
 
   useEffect(() => {
@@ -21,17 +24,18 @@ function App() {
   }, []);
 
   return (
-    <div className="overflow-y-auto min-h-[350px] max-h-[350px] min-w-[350px] max-w-[350px] p-2.5 bg-gray-100 ">
+    <div className="overflow-y-auto w-screen h-screen p-2.5 bg-gray-100 ">
       <h6 className="text-sm text-gray-500 font-bold uppercase mb-2.5">
         Recent
       </h6>
       <div className="grid grid-cols-2 items-start gap-2.5">
-        {clipboardItems.map((clipboardItem, index) => (
+        {snippets.map((snippet, index) => (
           <div
             key={index}
+            onClick={() => copySnippet(snippet)}
             className="rounded-md p-2.5 bg-white hover:bg-slate-50 text-sm cursor-pointer select-none"
           >
-            {clipboardItem}
+            {snippet}
           </div>
         ))}
       </div>
