5208b0dfa8269faf1b66df2e3af2fff3f7b1ac7a
Rain Tomista <rftomista@gmail.com>
2022-03-03 17:40:13 +0800

    Add TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <h1 className="text-6xl font-bold underline">
+    <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
   );
