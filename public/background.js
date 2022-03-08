adc24f7166c974a8906a23a68dfc0e3ee8968718
Rain Tomista <rftomista@gmail.com>
2022-03-08 17:47:50 +0800

    Add initial clipboard data (#9)

--- public/background.js
+++ public/background.js
@@ -1,4 +1,13 @@
+const INITIAL_APP_DATA = [
+  "Welcome to Gboard clipboard, any text that you copy will be saved...",
+  "Tap on a clip to paste it in the textbox",
+  "Use the edit icon to pin, add or delete clips",
+  "Touch and hold a clip to pin it. Unpinned clips will be deleted after 1 hours.",
+];
+
 chrome.runtime.onInstalled.addListener(async () => {
+  chrome.storage.local.set({ appData: INITIAL_APP_DATA });
+
   const tabs = await chrome.tabs.query({
     url: ["http://*/*", "https://*/*"],
   });
