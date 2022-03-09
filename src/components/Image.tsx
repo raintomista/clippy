78f00a5483a90cbd3697f0b5d0cf7fe6c2a79bb7
Rain Tomista <rftomista@gmail.com>
2022-03-09 10:43:38 +0800

    Implement click image snippet to copy

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { ImageProps } from "../common/types";
+import { getBlobFromUrl } from "../utils";
 
 function Image(props: ImageProps) {
   const { contentUrl } = props;
 
   const handleClick = async () => {
+    const blob = await getBlobFromUrl(contentUrl);
+    const clipboardItem = new ClipboardItem({
+      ["image/png"]: blob.slice(0, blob.size, "image/png"),
+    });
+
+    navigator.clipboard.write([clipboardItem]);
   };
 
   return (
