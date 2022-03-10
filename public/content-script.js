ec5e2935e73cb3e253141dd355e889c0f54358b3
Rain Tomista <rftomista@gmail.com>
2022-03-11 00:03:44 +0800

    Refactor content script into class (#16)

--- public/content-script.js
+++ public/content-script.js
@@ -9,11 +9,37 @@ function createSnippet(contentType, contentData) {
   };
 }
 
-document.addEventListener("copy", () => {
-  chrome.storage.local.get('appData', result => {
-    const selection = document.getSelection();
-    const snippet = createSnippet('text/plain', selection.toString())
-    const appData = [snippet, ...result.appData];
-    chrome.storage.local.set({ appData });
+function getAppData() {
+  return new Promise((resolve) => {
+    chrome.storage.local.get("appData", (result) => {
+      resolve(result.appData);
+    });
   });
-});
+}
+
+function setAppData(appData) {
+  chrome.storage.local.set({ appData });
+}
+
+class ContentScript {
+  constructor() {}
+
+  start() {
+    this.addClipboardListener();
+  }
+
+  addClipboardListener() {
+    document.addEventListener("copy", this.addSnippetToClipboard.bind(this));
+  }
+
+  async addSnippetToClipboard() {
+    const appData = await getAppData();
+    const selection = document.getSelection();
+    const snippet = createSnippet("text/plain", selection.toString());
+    appData.unshift(snippet);
+    setAppData(appData);
+  }
+}
+
+const contentScript = new ContentScript();
+contentScript.start();
