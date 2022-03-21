c75ad9324c5158d10f59855b13f464d087439392
Rain Tomista <rftomista@gmail.com>
2022-03-22 00:55:20 +0800

    Add discard selection button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { PencilIcon } from "@heroicons/react/solid";
+import { PencilIcon, XIcon } from "@heroicons/react/solid";
 import { Snippet } from "./common/types";
 import { IconButton, Image, PlainText } from "./components";
 import { getAppData, setAppData } from "./utils";
@@ -7,7 +7,14 @@ import { getAppData, setAppData } from "./utils";
 function App() {
   const [editable, setEditable] = useState<boolean>(false);
   const [selected, setSelected] = useState<string[]>([]);
-  const [snippets, setSnippets] = useState<Snippet[]>([]);
+  const [snippets, setSnippets] = useState<Snippet[]>([
+    {
+      id: new Date().getTime().toString(),
+      created_at: new Date().toISOString(),
+      content_type: "text/plain",
+      content_data: "haha",
+    },
+  ]);
 
   const initializeApp = async () => {
     const appData = await getAppData();
@@ -43,15 +50,25 @@ function App() {
   return (
     <div className="overflow-y-auto w-screen h-screen p-2.5 bg-gray-100 ">
       <div className="flex gap-2">
+        {editable && (
+          <IconButton
+            handleClick={toggleEdit}
+            iconComponent={<XIcon className="text-gray-500 select-none" />}
+          />
+        )}
         <h6 className="flex-1 text-sm text-gray-500 font-bold uppercase mb-2.5">
           {headerText}
         </h6>
-        <div className="flex gap-2">
-          <IconButton
-            handleClick={toggleEdit}
-            iconComponent={<PencilIcon className="text-gray-500 select-none" />}
-          />
-        </div>
+        {!editable && (
+          <div className="flex gap-2">
+            <IconButton
+              handleClick={toggleEdit}
+              iconComponent={
+                <PencilIcon className="text-gray-500 select-none" />
+              }
+            />
+          </div>
+        )}
       </div>
       <div className="grid grid-cols-2 items-start gap-2.5">
         {snippets.map((snippet, index) =>
