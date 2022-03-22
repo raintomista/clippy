b71143f111dbc14ea7232bc9df29b8690de0b5db
Rain Tomista <rftomista@gmail.com>
2022-03-22 13:41:59 +0800

    Make checkbox bigger

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -5,10 +5,10 @@ function Checkbox(props: CheckboxProps) {
   const { checked } = props;
   
   return (
-    <div className="absolute top-0 left-0 flex bg-gradient-to-r from-white to-transparent h-full p-2.5 pr-10">
+    <div className="absolute top-0 left-0 flex w-full h-full bg-gradient-to-r from-white to-transparent p-2.5">
       <input
         type="checkbox"
-        className="rounded-xl border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 transition duration-200 cursor-pointer"
+        className="w-5 h-5 rounded-xl border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 transition duration-200 cursor-pointer"
         checked={checked}
         readOnly
       />
