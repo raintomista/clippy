3f574934cf58006253d1bb22c8366d791d943372
Rain Tomista <rftomista@gmail.com>
2022-04-01 16:37:39 +0800

    Update storage utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,15 @@
 import { Snippet, Storage } from "../common/types";
 
-export function getAppData(): Promise<Snippet[]> {
+export function getLocalStorage(key: string): Promise<Snippet[]> {
   return new Promise((resolve) => {
-    chrome.storage.local.get("appData", (storage: Storage) => {
-      resolve(storage.appData ?? []);
+    chrome.storage.local.get(key, (storage: Storage) => {
+      resolve(storage[key] ?? []);
     });
   });
 }
 
-export function setAppData(appData: Snippet[]) {
-  chrome.storage.local.set({ appData });
+export function setLocalStorage(key: string, data: Snippet[]) {
+  chrome.storage.local.set({ [key]: data });
 }
 
 export async function getBlobFromUrl(url: string) {
